6b8561723ccfc6411ff75f8d1aa9e3bd630cba71
Rasmus <s152716@student.dtu.dk>
2019-10-12 00:15:05 +0200

    Nu kan man gætte ord

--- app/src/main/java/com/example/galgeleg/Spil_akt.java
+++ app/src/main/java/com/example/galgeleg/Spil_akt.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class Spil_akt extends AppCompatActivity implements View.OnClickListener {
@@ -13,11 +14,18 @@ public class Spil_akt extends AppCompatActivity implements View.OnClickListener 
     Galgelogik logik = new Galgelogik();
 
     Button[] tastatur;
+    EditText felt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spil);
+
+        felt = findViewById(R.id.felt);
+
+
+        logik.nulstil();
 
         tastatur = new Button[29];
 
@@ -25,7 +33,7 @@ public class Spil_akt extends AppCompatActivity implements View.OnClickListener 
         char hej = 65;
 
         //setOnClickListener laves til hver button i tastaturet
-        for(int i = 0; i < tastatur.length - 4; i++){
+        for(int i = 0; i < tastatur.length - 3; i++){
             String næsteKnap = "knap" + hej;
             hej++;
             int knapId = getResources().getIdentifier(næsteKnap, "id", getPackageName());
@@ -34,17 +42,29 @@ public class Spil_akt extends AppCompatActivity implements View.OnClickListener 
 
         }
         //Æ, Ø, Å hardcodes
-        tastatur[27] = findViewById(R.id.knapÆ);
+        tastatur[26] = findViewById(R.id.knapÆ);
+        tastatur[26].setOnClickListener(this);
+        tastatur[27] = findViewById(R.id.knapØ);
         tastatur[27].setOnClickListener(this);
-        tastatur[28] = findViewById(R.id.knapØ);
+        tastatur[28] = findViewById(R.id.knapÅ);
         tastatur[28].setOnClickListener(this);
-        tastatur[29] = findViewById(R.id.knapÅ);
-        tastatur[29].setOnClickListener(this);
+
 
     }
 
     @Override
     public void onClick(View v) {
+
+        //Tjekker hvad der trykkes på
+        for(int i=0; i<tastatur.length; i++){
+            if(v == tastatur[i]){
+
+                logik.gætBogstav(tastatur[i].getText().toString().toLowerCase());
+                logik.logStatus();
+                //Opdater ord-feltet, når der gættes
+                felt.setText(logik.getSynligtOrd());
+            }
+        }
 
 
     }
